0f9489e362878f768c539138d356da0b2a9768d1
James Ots <code@jamesots.com>
2014-03-11 22:53:54 +0000

    Working implementation of PBKDF2

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ This is a small utility to help with hashing passwords.
 
 Example:
 
-    // create a hasher with the default salt generator and hash (SHA256)
+  // create a hasher with the default salt generator and hash (SHA256)
 	var hasher = new PasswordHasher();
 
 	// the password will be hashed with 32 bytes of randomly generated salt
@@ -14,3 +14,13 @@ Example:
 	// the returned hash contains the salt and the hashed password, and can be
 	// checked against a password using checkPassword():
 	var passwordOk = hasher.checkPassword(hash, "mypassword");
+
+It also implements the PBKDF2 algorithm:
+ 
+  // create a key generator with a particular hash (default is SHA1)
+  var pbkdf2 = new PBKDF2(hash: new SHA256());
+  
+  // generate a key with the given password and salt, using
+  // 1000 iterations of the hash function, and giving a 20 byte key
+  pbkdf2.generateKey("password", "salt", 1000, 20);
+  
--- lib/pbkdf2.dart
+++ lib/pbkdf2.dart
@@ -1,21 +1,63 @@
 library pbkdf2;
 
-import 'dart:math';
+import 'package:crypto/crypto.dart';
 
-List<int> hash(String password, String salt, int c, int dkLen) {
-  var hLen = ((2 << 31) - 1) * 32;
-  if (dkLen > hLen) {
-    throw "derived key too long";
-    
-    var l = (dkLen / hLen).ceil();
-    var r = dkLen - (l - 1) * hLen;
-    
-    for (var i = 0; i < l; i++) {
-      var t = F(password, salt, c, i);
+class PBKDF2 {
+  Hash hash;
+
+  PBKDF2({Hash this.hash});
+
+  List<int> generateKey(String password, String salt, int c, int dkLen) {
+    var PRF = hash.newInstance();
+    PRF.add([1, 2, 3]);
+    var blockSize = PRF.close().length;
+    if (dkLen > ((2 << 31) - 1) * blockSize) {
+      throw "derived key too long";
+    }
+
+    var numberOfBlocks = (dkLen / blockSize).ceil();
+    var sizeOfLastBlock = dkLen - (numberOfBlocks - 1) * blockSize;
+
+    var key = [];
+    for (var i = 1; i <= numberOfBlocks; i++) {
+      var block = _computeBlock(password, salt, c, i);
+      if (i < numberOfBlocks) {
+        key.addAll(block);
+      } else {
+        key.addAll(block.sublist(0, sizeOfLastBlock));
+      }
+    }
+    return key;
+  }
+
+  List<int> _computeBlock(String password, String salt, int iterations, int blockNumber) {
+    var hmac = new HMAC(hash, password.codeUnits);
+    hmac.add(salt.codeUnits);
+    writeBlockNumber(hmac, blockNumber);
+    var lastDigest = hmac.close();
+    var result = lastDigest;
+    for (var i = 1; i < iterations; i++) {
+      hmac = new HMAC(hash, password.codeUnits);
+      hmac.add(lastDigest);
+      var newDigest = hmac.close();
+      xorLists(result, newDigest);
+      lastDigest = newDigest;
+    }
+    return result;
+  }
+  
+  writeBlockNumber(HMAC hmac, int blockNumber) {
+    var list = [];
+    list.add(blockNumber >> 24);
+    list.add(blockNumber >> 16);
+    list.add(blockNumber >> 8);
+    list.add(blockNumber);
+    hmac.add(list);
+  }
+  
+  xorLists(List<int> list1, List<int> list2) {
+    for (var i = 0; i < list1.length; i++) {
+      list1[i] = list1[i] ^ list2[i];
     }
   }
-}
-
-F(String password, String salt, int c, int i) {
-  
 }
